159c5f4126700efe2fd75e463b4785edcc30b232
Harry Dodson <harry.lee.dodson@gmail.com>
2024-05-19 06:54:49 -0400

    window size, the easy way

--- kilo.c
+++ kilo.c
@@ -1,9 +1,11 @@
 /*** includes ***/
 
+#include <asm-generic/ioctls.h>
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -13,7 +15,9 @@
 /*** data ***/
 
 struct editorConfig {
-    struct termios orig_termios;
+  int screenrows;
+  int screencols;
+  struct termios orig_termios;
 };
 
 struct editorConfig E;
@@ -60,11 +64,23 @@ char editorReadKey() {
   return c;
 }
 
+int getWindowSize(int *rows, int *cols) {
+  struct winsize ws;
+
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    return -1;
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
 /** output **/
 
 void editorDrawRows() {
   int y;
-  for (y = 0; y < 24; y++) {
+  for (y = 0; y < E.screenrows; y++) {
     write(STDOUT_FILENO, "~\r\n", 3);
   }
 }
@@ -93,8 +109,14 @@ void editorProcessKeypress() {
 
 /*** init ***/
 
+void initEditor() {
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1)
+    die("getWindowSize");
+}
+
 int main() {
   enableRawMode();
+  initEditor();
 
   while (1) {
     editorRefreshScreen();
